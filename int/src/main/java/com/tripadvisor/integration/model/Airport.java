8beba2d19145334a73d8bbe7b3576a649c5497ea
Musiliu Bolaji <mabolaji@mum.edu>
2019-07-16 17:09:19 -0500

    cascade dropdowns

--- int/src/main/java/com/tripadvisor/integration/model/Airport.java
+++ int/src/main/java/com/tripadvisor/integration/model/Airport.java
@@ -1,6 +1,14 @@
 package com.tripadvisor.integration.model;
 
+import lombok.Data;
+
+@Data
 public class Airport {
     private  Long id;
     private  String name;
+
+    @Override
+    public String toString() {
+        return  name ;
+    }
 }

3288d15974455b83386c7bdd2abca422f0ee3d89
Musiliu Bolaji <mabolaji@mum.edu>
2019-07-16 21:37:00 -0500

    build files

---
+++ flightBooking/target/classes/import.sql
@@ -0,0 +1,36 @@
+# //Flight
+# INSERT INTO `flightbook`.`flight` (`arrival_date`, `departure_date`, `flight_number`, `airlines_id`, `airplane_id`, `arrival_id`, `departure_id`) VALUES ('2019-07-25 00:00:00', '2019-07-14 00:00:00', 'Flight001', '1', '1', '3', '5');
+# INSERT INTO `flightbook`.`flight` (`arrival_date`, `departure_date`, `flight_number`, `airlines_id`, `airplane_id`, `arrival_id`, `departure_id`) VALUES ('2019-07-23 00:00:00', '2019-07-22 00:00:00', 'Flight002', '2', '4', '7', '4');
+# INSERT INTO `flightbook`.`flight` (`arrival_date`, `departure_date`, `flight_number`, `airlines_id`, `airplane_id`, `arrival_id`, `departure_id`) VALUES ('2019-07-25 00:00:00', '2019-07-14 00:00:00', 'Flight003', '2', '4', '3', '5');
+# INSERT INTO `flightbook`.`flight` (`arrival_date`, `departure_date`, `flight_number`, `airlines_id`, `airplane_id`, `arrival_id`, `departure_id`) VALUES ('2019-07-23 00:00:00', '2019-07-22 00:00:00', 'Flight004', '3', '6', '7', '4');
+# INSERT INTO `flightbook`.`flight` (`arrival_date`, `departure_date`, `flight_number`, `airlines_id`, `airplane_id`, `arrival_id`, `departure_id`) VALUES ('2019-07-25 00:00:00', '2019-07-14 00:00:00', 'Flight005', '3', '6', '3', '5');
+# INSERT INTO `flightbook`.`flight` (`arrival_date`, `departure_date`, `flight_number`, `airlines_id`, `airplane_id`, `arrival_id`, `departure_id`) VALUES ('2019-07-23 00:00:00', '2019-07-22 00:00:00', 'Flight006', '2', '2', '7', '4');
+# //airplanes
+# INSERT INTO `flightbook`.`airplanes` (`regestration_no`, `seat`, `airline_id`) VALUES ('airplane001', '100', '1');
+# INSERT INTO `flightbook`.`airplanes` (`regestration_no`, `seat`, `airline_id`) VALUES ('airplane002', '150', '1');
+# INSERT INTO `flightbook`.`airplanes` (`regestration_no`, `seat`, `airline_id`) VALUES ('airplane003', '200', '1');
+# INSERT INTO `flightbook`.`airplanes` (`regestration_no`, `seat`, `airline_id`) VALUES ('airplane004', '250', '2');
+# INSERT INTO `flightbook`.`airplanes` (`regestration_no`, `seat`, `airline_id`) VALUES ('airplane005', '200', '2');
+# INSERT INTO `flightbook`.`airplanes` (`regestration_no`, `seat`, `airline_id`) VALUES ('airplane006', '250', '3');
+# INSERT INTO `flightbook`.`airplanes` (`regestration_no`, `seat`, `airline_id`) VALUES ('airplane007', '100', '3');
+# INSERT INTO `flightbook`.`airplanes` (`regestration_no`, `seat`, `airline_id`) VALUES ('airplane008', '250', '4');
+# INSERT INTO `flightbook`.`airplanes` (`regestration_no`, `seat`, `airline_id`) VALUES ('airplane010', '150', '4');
+#
+# //Airline
+# INSERT INTO `flightbook`.`airlines` (`name`) VALUES (null,'American Airlines');
+# INSERT INTO `flightbook`.`airlines` (`name`) VALUES (null,'Rwandan Airlines');
+# INSERT INTO `flightbook`.`airlines` (`name`) VALUES (null,'Ethiopian Airlines');
+# INSERT INTO `flightbook`.`airlines` (`name`) VALUES (null,'Lagos Airlines');
+#
+#
+# //Airport
+# INSERT INTO `flightbook`.`airport` (`airport_code`, `city`, `country`, `name`)  VALUES (null,'airport001', 'Addis Abeba', 'Ethiopian', 'Addis Airport');
+# INSERT INTO `flightbook`.`airport` (`airport_code`, `city`, `country`, `name`)  VALUES (null,'airport002', 'kigali', 'Rwanda', 'Kanombe');
+# INSERT INTO `flightbook`.`airport` (`airport_code`, `city`, `country`, `name`)  VALUES (null,'airport003', 'Lagos', 'Nigeria', 'Lagos Airport');
+# INSERT INTO `flightbook`.`airport` (`airport_code`, `city`, `country`, `name`)  VALUES (null,'airport004', 'DC', 'USA', 'DC Airport');
+# INSERT INTO `flightbook`.`airport` (`airport_code`, `city`, `country`, `name`)  VALUES (null,'airport005', 'Chicago', 'USA', 'Chicago Airport');
+# INSERT INTO `flightbook`.`airport` (`airport_code`, `city`, `country`, `name`)  VALUES (null,'airport006', 'Toronto', 'Canda', 'Toronto Airport');
+# INSERT INTO `flightbook`.`airport` (`airport_code`, `city`, `country`, `name`) VALUES (null,'airport007', 'Bruxelles', 'Belgium', 'Bruxelles Airport');
+#
+
+

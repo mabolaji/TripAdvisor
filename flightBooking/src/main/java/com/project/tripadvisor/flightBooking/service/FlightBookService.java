78e2b3a25a590f2cfe6509dfbad350b3ac63d6a2
daveabipomy <abeba.ibrahim@gmail.com>
2019-07-16 21:36:48 -0700

    newFlight
    newFlight

---
+++ flightBooking/src/main/java/com/project/tripadvisor/flightBooking/service/FlightBookService.java
@@ -0,0 +1,20 @@
+package com.project.tripadvisor.flightBooking.service;
+
+import com.project.tripadvisor.flightBooking.model.Airport;
+import com.project.tripadvisor.flightBooking.model.Flight;
+import com.project.tripadvisor.flightBooking.model.FlightBook;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface FlightBookService {
+
+    public List<Flight> flightes(Long departure, Long arrival, LocalDate departureDate, LocalDate arrivalDate);
+
+    public FlightBook bookFlight(String email, Flight flight);
+    public  List<Flight> flightesFilter(Long departure, Long arrival, LocalDate departureDate);
+    public List<Airport> departureFlight(Long departure);
+    public List<Flight> ActualFight();
+}
